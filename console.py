d411432b1fbf6d01dd1e6db28fe07be4bf8ad401
ammarelbadry1 <ammarelbadry1@gmail.com>
2023-12-07 16:19:34 +0200

    AIR-3 [update] basic console fuctionality, [add] docstrings

--- console.py
+++ console.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
+"""console module/program
+
+This module defines HBNBCommand child class
+that inherits the command interpreter functionality
+from the super class Cmd.
+"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """Simple command processor example."""
+    """Represent a HBNBCommand class
+
+    Attributes:
+        prompt (str): the string issued to solicit input
+    """
 
     prompt = "(hbnb) "
 
+    # ----- Overwritten super class methods -----
     def emptyline(self):
-        print(end='')
-    def do_greet(self, line):
-        """print hello"""
-        print("hello")
+        """Makes nothing when the input is empty line
+        """
+        pass
 
+    def postloop(self):
+        """Prints a new line when the cmdloop return
+        """
+        print()
+
+    # ----- Basic console commands -----
     def do_EOF(self, line):
-        """exit the programm"""
+        """EOF command to exit the program
+        """
         return True
 
     def do_quit(self, line):
-        """exit the program"""
+        """Quit command to exit the program
+        """
         return True
 
 
